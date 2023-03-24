543110256148e79408b760806a8493cab8faf080
tzolsch <tzolsch@googlemail.com>
2023-03-24 15:41:21 +0100

    TOKEN CACHE

--- OSCspawn.py
+++ OSCspawn.py
@@ -26,6 +26,7 @@ except FileNotFoundError:
 # GTP finetuning prompt - needed for custom AI response thread only
 GTP_PROMPT_HEAD = ""
 
+
 # ------------------------------------- global speech-to-text related variables ----------------
 
 MAX_INTRA_PHRASE_PAUSE = 2.0
@@ -58,6 +59,7 @@ OPEN_PORTS = {}
 # language model:
 # either "google" (online) or "facebook" (offlline) -> 'm2m_100_1.28M' model - facebook needs pytorch (required by EasyNMT)
 TR_ENGINE = 'GPT'
+TOKEN_CACHE = 1000
 
 # --------- global text-to-speech related variables -------------
 
@@ -78,17 +80,50 @@ elif TR_ENGINE == 'facebook':
     trans_langs = model.get_languages()
     trans_func = lambda txt, src, trg: model.translate(txt, source_lang=src, target_lang=trg)
 elif TR_ENGINE == 'GPT':
-    trans_langs = ['english', 'spanish', 'italian', 'russian', 'polish', 'greek', 'dutch', 'romanian', 'portuguese', 'hebrew', 'arabic']
+    trans_langs = ['english', 'spanish', 'italian', 'russian', 'polish', 'greek', 'dutch', 'romanian', 'portuguese', 'arabic']
+    #trans_langs = ['english', 'spanish', 'italian', 'russian', 'chinese']
     trans_prompt = lambda x,y,z: f'Translate from {x} to {y}:\n {z} => '
     def trans_func(txt, src, trg):
+        global TOKEN_CACHE
+        prompt = trans_prompt(src, trg, txt)
+        tok_estimate = len([w for w in prompt.split(' ') if w != ' '])
+        max_tokens = int(max(2040 - (TOKEN_CACHE + 10), 0))
         print(trans_prompt(src, trg, txt))
-        result = (openai.Completion.create(model="text-curie-001", prompt=trans_prompt(src, trg, txt), temperature=0.3, max_tokens=4*len(txt), top_p=1.0, frequency_penalty=0.0, presence_penalty=0.0)).choices[0].text.replace('\n', '')
-        print(result)
+        result = openai.Completion.create(model="text-curie-001", prompt=trans_prompt(src, trg, txt),
+                                           max_tokens=max_tokens, temperature=0.3, top_p=1.0, frequency_penalty=0.0,
+                                           presence_penalty=0.0)
+        TOKEN_CACHE = result['usage']['completion_tokens']
+        result = result.choices[0].text.replace('\n', '')
+        print(f'TK={TOKEN_CACHE}')
         return result
 
 # ----------------- library of functions that can be triggered ------------------------------
 
 
+def chainTransWorkFunc(txt, lang_list):
+    txt_chain = [txt]
+    if len(lang_list) == 2:
+        txt_chain += [txt]
+    else:
+        for k in range(len(lang_list) - 1):
+            txt_chain += [trans_func(txt=txt_chain[k], src=lang_list[k], trg=lang_list[k + 1])]
+            # txt_chain += [translator.translate(txt_chain[k], src=lang_list[k], dest=lang_list[k+1]).text]
+    result = []
+    for k in range(len(txt_chain)):
+        result += (lang_list[k],)
+        result += (txt_chain[k],)
+    return tuple(result)
+
+def langSampler(txt, clen):
+    source_l = 'de' if TR_ENGINE != 'GPT' else 'german'
+    in_txt = txt
+    chain_len = clen
+    lang_list = [source_l]
+    lang_sample = random.sample([k for k in range(len(trans_langs))], chain_len)
+    for k in lang_sample:
+        lang_list += [trans_langs[k]]
+    lang_list += [source_l]
+    return lang_list
 
 def openDmmPort(unused_addr, args):
     """
@@ -221,15 +256,10 @@ def chainTrans(unused_addrs, args):
     Translates string args[0] through the languages indicated by a list of lang codes
     in args[1].
     """
-    source_l = 'de' if TR_ENGINE != 'GPT' else 'german'
-    in_txt = args[0]
-    chain_len = args[1]
-    lang_list = [source_l]
-    lang_sample = random.sample([k for k in range(len(trans_langs))], chain_len)
-    for k in lang_sample:
-        lang_list += [trans_langs[k]]
-    lang_list += [source_l]
-    thread_obj = Thread(target=chainTransThread, args=(in_txt, lang_list,), daemon=True)
+    global TOKEN_CACHE
+    TOKEN_CACHE = 1000
+    lang_list = langSampler(args[0], args[1])
+    thread_obj = Thread(target=chainTransThread, args=(args[0], lang_list,), daemon=True)
     thread_obj.start()
     return
 
@@ -239,18 +269,7 @@ def chainTransThread(txt, lang_list):
     worker thread spawned by chainTrans to do the translation and send results through osc client with
     "/chainTransResult" tag.
     """
-    txt_chain = [txt]
-    if len(lang_list) == 2:
-        txt_chain += [txt]
-    else:
-        for k in range(len(lang_list) - 1):
-            txt_chain += [trans_func(txt=txt_chain[k], src=lang_list[k], trg=lang_list[k+1])]
-            # txt_chain += [translator.translate(txt_chain[k], src=lang_list[k], dest=lang_list[k+1]).text]
-    result = []
-    for k in range(len(txt_chain)):
-        result += (lang_list[k],)
-        result += (txt_chain[k],)
-    result = tuple(result)
+    result = chainTransWorkFunc(txt, lang_list)
     client.send_message('/chainTransResult', result)
     return
 
@@ -278,25 +297,33 @@ def ttsThread(args):
     return
 
 
-def gpt(unused_addrs, msg, temp, freq, pres, tok, role, eng):
+def gpt(unused_addrs, msg, temp, freq, pres, tok, role, eng, trans):
     txt = ROLES[role] + msg
-
-    thread_obj = Thread(target=gptThread, args=(txt, temp, freq, pres, tok, eng), daemon=True)
+    thread_obj = Thread(target=gptThread, args=(txt, temp, freq, pres, tok, eng, trans), daemon=True)
     thread_obj.start()
     return
 
-def gptThread(txt, temp, freq, pres, tok,eng):
+def gptThread(txt, temp, freq, pres, tok,eng, trans):
+    global TOKEN_CACHE
+    if tok < 0:
+        tok = max(2040 - (TOKEN_CACHE + 10), 0)
+
     engine = ['ada',
               'babbage',
-              'curie',
               'text-curie-001',
               'text-davinci-003']
     try:
         response = openai.Completion.create(engine=engine[eng], prompt=txt, temperature=temp, top_p=1,
                                             max_tokens=tok, frequency_penalty=freq, presence_penalty=pres)
-        print('GPT_PROMPT = ' + txt)
+        TOKEN_CACHE = response['usage']['completion_tokens']
         print('GPT_RESPONSE = ' + response.choices[0].text)
-        client.send_message('/GPT', response.choices[0].text.replace('\n', ' '))
+        if trans > 0:
+            lang_list = langSampler(response.choices[0].text.replace('\n', ' '), trans)
+            chain_end = chainTransWorkFunc(response.choices[0].text.replace('\n', ' '), lang_list)
+            out = chain_end[-1]
+        else:
+            out = response.choices[0].text.replace('\n', ' ')
+        client.send_message('/GPT', out)
     except RateLimitError:
         print('GPT funds depleted!')
         client.send_message('/GPT', txt)
